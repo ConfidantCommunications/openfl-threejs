86eff53475d215d1fe6da1304f68e760899eb30d
Allan Dowdeswell <secrets@confidant.ca>
2025-12-30 11:23:00 -0600

    Cleanup.

--- examples/HelloThreejs/Source/Main.hx
+++ examples/HelloThreejs/Source/Main.hx
@@ -1,6 +1,7 @@
 package;
 
 import openfl.display.Sprite;
+import openfl.display.*;
 import openfl.display3D.Context3D;
 import openfl.display.Stage3D;
 import openfl.events.Event;
@@ -19,26 +20,18 @@ class Main extends Sprite
 		super();
 		
 
-		if( stage.stage3Ds.length > 0 )
-			{
-				var stage3D:Stage3D = stage.stage3Ds[0];
-				stage3D.addEventListener( Event.CONTEXT3D_CREATE, myContext3DHandler );
-				stage3D.requestContext3D( );
-			}
-	
+		if( stage.stage3Ds.length > 0 ) {
+			var stage3D:Stage3D = stage.stage3Ds[0];
+			stage3D.addEventListener( Event.CONTEXT3D_CREATE, myContext3DHandler );
+			stage3D.requestContext3D( );
+		}
 			
 	}
 	private function myContext3DHandler ( event : Event ) : Void {
 		var targetStage3D : Stage3D = cast event.target;
-		// InitAll3DResources( targetStage3D.context3D );
-		// StartRendering( targetStage3D.context3D );
 		var webGLRenderContext:Dynamic = @:privateAccess targetStage3D.context3D.gl;
-		/* cn.getContextAttributes = function() {
-			return {
-				alpha:1
-			}
-		} */
 		init(webGLRenderContext);
+		// addGraphics();
 	}
 	public function init(context3D:Context3D):Void
 	{
@@ -46,10 +39,6 @@ class Main extends Sprite
 		var width = stage.stageWidth;
 		var height = stage.stageHeight;
 
-
-		// var stage3DManager = Stage3DManager.getInstance(stage);
-		
-		// init
 		
 		camera = new PerspectiveCamera( 70, width / height, 0.01, 10 );
 		camera.position.z = 1;
@@ -70,25 +59,22 @@ class Main extends Sprite
 		renderer = untyped new WebGLRenderer({
 			antialias: true,
 			context: context3D,
-			// context: untyped stage3DManager.getFreeStage3DProxy().context3D,
-			// context: untyped stage3DManager.getFreeStage3DProxy().requestContext3D(),
 			powerPreference: "high-performance",
 			failIfMajorPerformanceCaveat: false,
 			alpha: true
 		});
 		
-		// var renderer = new THREE.WebGLRenderer( { antialias: true } );
 		renderer.setSize( width, height );
 		renderer.setAnimationLoop( cast animate );
-		
-		// , stage3DManager:Stage3DManager
-		// document.body.appendChild( renderer.domElement );
-
-
-		// var threeView = new ThreeView(scene, renderer, stage3DManager);
-		// addChild(threeView);
 	}
-	// animation
+	private function addGraphics():Void
+	{
+		var spr:Sprite = new Sprite();
+		spr.graphics.beginFill(0x4545CD, 1);
+		spr.graphics.drawRect(0, 0, 100,100);
+		spr.graphics.endFill();
+		addChild(spr);
+	}
 	
 	private function animate( time ) {
 	
